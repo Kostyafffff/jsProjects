bd8892985a99291f6c6926b7bc3b8f5e3d5b9480
kostya <kostyan68@gmail.com>
2019-05-26 22:55:14 +0300

    refactor 2 functions

--- simpleCalc/main.js
+++ simpleCalc/main.js
@@ -2,58 +2,6 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
 const display = document.querySelector(".calculator__display");
 
-keys.addEventListener('click', e => {
-
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-        const previousKeyType = calculator.dataset.previousKeyType;
-        if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
-                display.textContent = keyContent;
-            } else {
-                display.textContent = displayedNum + keyContent;
-            }
-            calculator.dataset.previousKeyType = 'number';
-        }
-        if (action === 'decimal') {
-            if (!displayedNum.includes('.')) {
-                display.textContent = displayedNum + '.';
-            } else if (previousKeyType === 'operator') {
-                display.textContent = '0.';
-            }
-            calculator.dataset.previousKeyType = 'decimal';
-        }
-
-        if (action === 'clear') {
-            calculator.dataset.previousKeyType = 'clear';
-        }
-        if (action === 'calculate') {
-            const firstValue = displayedNum;
-            const operator = calculator.dataset.operator;
-            const secondValue = displayedNum;
-
-            display.textContent = calculate(firstValue, operator, secondValue);
-            calculator.dataset.previousKeyType = 'decimal';
-        }
-
-        if (
-            action === 'add' ||
-            action === 'substract' ||
-            action === 'multiply' ||
-            action === 'divide') {
-            key.classList.add('is-depressed');
-            calculator.dataset.previousKeyType = 'operator';
-            calculator.dataset.firstValue = displayedNum;
-            calculator.dataset.operator = action;
-        }
-
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'));
-    }
-});
 const calculate = (n1, operator, n2) => {
     let result = "";
     switch (operator) {
@@ -71,4 +19,61 @@ const calculate = (n1, operator, n2) => {
             break;
     }
     return result;
+};
+
+const getKeyType = key => {
+    const {
+        action
+    } = key.dataset;
+    if (!action) return 'number';
+    if (action === 'add' ||
+        action === 'substract' ||
+        action === 'multiply' ||
+        action === 'divide'
+    ) return 'operator';
+    return action;
+};
+
+const createResultString = (key, diplayedNum, state) => {
+    const keyContent = key.textContent;
+    const keyType = getKeyType(key);
+    const {
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType
+    } = state;
+
+    if (keyType === 'number') {
+        return diplayedNum === '0' ||
+            previousKeyType === 'operator' ||
+            previousKeyType === 'calculate' ?
+            keyContent :
+            displayedNum + keyContent;
+    }
+
+    if (keyType === 'decimal') {
+        if (!displayedNum.includes('.')) return displayedNum + '.';
+        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.';
+        return diplayedNum;
+    }
+
+    if (keyType === 'operator') {
+        return firstValue &&
+            operator &&
+            previousKeyType !== 'operator' &&
+            previousKeyType !== 'calculate' ?
+            calculate(firstValue, operator, displayedNum) :
+            displayedNum;
+    }
+
+    if (keyType === 'clear') return 0;
+
+    if (keyType === 'calculate') {
+        return firstValue ?
+            previousKeyType === 'calculate' ?
+            calculate(displayedNum, operator, modValue) :
+            calculate(firstValue, operator, displayedNum) :
+            diplayedNum;
+    }
 };
