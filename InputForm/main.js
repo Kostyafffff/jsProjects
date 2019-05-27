e5bb3ae52a4812b767b2dedc707a8816148a278d
kostya <kostyan68@gmail.com>
2019-05-27 23:11:30 +0300

    SIMPLE INPUT FORM

---
+++ InputForm/main.js
@@ -0,0 +1,49 @@
+const x = document.getElementById("form_sample");
+
+const createForm = document.createElement('form');
+
+createForm.setAttribute("action", "");
+
+createForm.setAttribute("method", "post");
+
+x.appendChild(createForm);
+
+let heading = document.createElement('h2');
+heading.innerHTML = "Contact Form";
+createForm.appendChild(heading);
+
+let line = document.createElement('hr');
+createForm.appendChild(line);
+
+let linebreak = document.createElement('br');
+createForm.appendChild(linebreak);
+
+let emaillabel = document.createElement('label');
+emaillabel.innerHTML = "Your Email : ";
+createForm.appendChild(emaillabel);
+
+let emailelement = document.createElement('input');
+emailelement.setAttribute("type", "text");
+emailelement.setAttribute("name", "demail");
+createForm.appendChild(emailelement);
+
+let emailbreak = document.createElement('br');
+createForm.appendChild(emailbreak);
+
+let messageLabel = document.createElement('label');
+messageLabel.innerHTML = "Your Message : ";
+createForm.appendChild(messageLabel);
+
+let textAreaElement = document.createElement('textarea');
+textAreaElement.setAttribute("name", "dmessage");
+createForm.appendChild(textAreaElement);
+
+let messageBreak = document.createElement('br');
+createForm.appendChild(messageBreak);
+
+//submit button
+let submitBtnElement = document.createElement('input');
+submitBtnElement.setAttribute("type", "submit");
+submitBtnElement.setAttribute("name", "dsubmit");
+submitBtnElement.setAttribute("value", "Submit");
+createForm.appendChild(submitBtnElement);
