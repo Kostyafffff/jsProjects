428b71699dd5ec294125457385e21bc0df87bd44
kostya <kostyan68@gmail.com>
2019-06-05 23:20:41 +0300

    add millis

--- FirstClock/jsFile.js
+++ FirstClock/jsFile.js
@@ -3,6 +3,7 @@ setInterval(function () {
     let hours = currentTime.getHours();
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
+    let millis = currentTime.getMilliseconds();
     let period = "AM";
 
     if (hours >= 12) {
@@ -18,9 +19,10 @@ setInterval(function () {
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    let clockTime = hours + ":" + minutes + ":" + seconds;
+
+    let clockTime = hours + ":" + minutes + ":" + seconds + ":" + millis;
 
     let clock = document.getElementById('clock');
     clock.innerText = clockTime;
 
-}, 1000);
+}, 250);
