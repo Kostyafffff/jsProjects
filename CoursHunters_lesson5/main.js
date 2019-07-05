18f2fec9a8c441f3547cc9025143337b5c618dfd
kostya <kostyan68@gmail.com>
2019-07-06 02:05:08 +0300

    end day commit

--- CoursHunters_lesson5/main.js
+++ CoursHunters_lesson5/main.js
@@ -5,6 +5,7 @@ let tasks = [
 ];
 
 let ul = document.querySelector(".list-group");
+let deleteButtons = document.getElementsByClassName("delete-item");
 
 function generateList(tasksArray) {
 
@@ -13,13 +14,21 @@ function generateList(tasksArray) {
     for (let i = 0; i < tasks.length; i++) {
         ul.appendChild(listTemplate(tasksArray[i]));
     }
+
+    setDeleteEvent();
 }
 
 function listTemplate(task) {
+    //create list item
     let li = document.createElement("li");
     li.textContent = task;
-    li.className = "list-group-item";
+    li.className = "list-group-item d-flex align-items-center";
+    // create tag i fa-thrash-item
+    let iDelete = document.createElement("i");
+    iDelete.className = "fas fa-trash-alt delete-item ml-auto";
 
+    // append delete iconE
+    li.appendChild(iDelete);
     return li;
 }
 
@@ -27,8 +36,24 @@ function clearList() {
     ul.innerHTML = "";
 }
 
+
 function addList(list) {
     tasks.unshift(list);
     generateList(tasks);
 }
 generateList(tasks);
+
+let btn = document.querySelector(".clear-btn");
+
+function setDeleteEvent() {
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', function (e) {
+            console.log("click");
+        });
+    }
+}
+
+function onClick(e) {
+    console.log('click');
+}
+btn.addEventListener("click", onClick);
