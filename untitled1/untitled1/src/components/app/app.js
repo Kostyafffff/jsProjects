2ccdfe4eec897fc0c2d6436d49f5be1fe380432a
Kostya Furmanov <kostyaf@123software.ru>
2019-11-19 14:56:42 +0200

    day commit

--- untitled1/untitled1/src/components/app/app.js
+++ untitled1/untitled1/src/components/app/app.js
@@ -5,30 +5,28 @@ import SearchPannel from '../search-panel'
 import ToDoList from '../todo-list/todo-list'
 import ItemStatusFilter from '../item-status-filter'
 
+const toDoData = [
+    {
+        label : 'Drink Cofee', important : false, id : 1
+    },
+    {
+        label : 'Make Awsome App', important : true, id : 2
+    },
+    {
+        label : 'Have a lunch', important : false, id : 3
+    }
+];
 
-const App = () => {
-    const toDoData = [
-        {
-            label : 'Drink Cofee', important : false, id : 1
-        },
-        {
-            label : 'Make Awsome App', important : true, id : 2
-        },
-        {
-            label : 'Have a lunch', important : false, id : 3
-        }
-    ];
-    return (
-        <div>
-            <ItemStatusFilter />
-            <SearchPannel />
-            <AppHeader />
-            <ToDoList todos = {toDoData}
+const App = () => (
+    <div>
+        <ItemStatusFilter />
+        <SearchPannel />
+        <AppHeader />
+        <ToDoList
+            todos = {toDoData}
             onDeleted={(id) => console.log('del', id)}
-            />
-        </div>
-    );
-
-};
+        />
+    </div>
+);
 
 export default App;
--- untitled1/untitled1/src/components/search-panel/search-panel.js
+++ untitled1/untitled1/src/components/search-panel/search-panel.js
@@ -3,8 +3,7 @@ import React from "react";
 const SearchPannel = () => {
     return (
 
-        <input
-            placeholder = "search" />
+        <input placeholder = "search" />
     );
 };
 
