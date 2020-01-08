d9f47575998eee36fdedd96bda43ec728d810033
kostya <kostyan68@gmail.com>
2020-01-08 21:49:58 +0200

    upd

---
+++ TypeScriptHomePrj/app.ts
@@ -0,0 +1,62 @@
+function autobind(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+    ){
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get(){
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+    }
+class ProjectInput{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor(){
+        this.templateElement = <HTMLTemplateElement> document
+            .getElementById('project-input')! as HTMLTemplateElement;
+
+
+
+        this.hostElement = <HTMLDivElement> document
+            .getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
+        this.attach();
+
+    };
+
+    // @ts-ignore
+    @autobind
+    private submitHandler(event: Event){
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    private attach(){
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
