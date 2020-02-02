20fd1303063e9d28a7a1229c129fc79a5c472b89
kostya <kostyan68@gmail.com>
2020-02-03 00:04:05 +0200

    day commit

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,23 +1,28 @@
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rand } from "./actions";
-const { dispatch } = store;
-
-const bindActionCreator = (creator, dispatch) => (...args) => {
-    dispatch(creator(...args));
-};
-
-const incDispatch = () => bindActionCreator(inc, dispatch);
-const decDispatch = () => bindActionCreator(dec, dispatch);
-const rndDispatch = payload => bindActionCreator(rnd, dispatch);
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-document.getElementById('dec').addEventListener('click', decDispatch);
+const {
+    inc,
+    dec,
+    rnd,
+    } = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
 
-document.getElementById('rnd').addEventListener('click', rndDispatch);
+document.getElementById('dec')
+    .addEventListener('click', dec);
+
+document.getElementById('inc')
+    .addEventListener('click', inc);
+
+document.getElementById('rnd')
+    .addEventListener('click', ()=> {
+    const payload = Math.floor(Math.random() * 10);
+    rnd(payload);
+});
 
 const update = () => {
     document.getElementById('counter').innerHTML = store.getState();
