b680731da809d8fd6eb2c80964b1fd7ab13825bb
kostya <kostyan68@gmail.com>
2021-02-07 13:05:29 +0200

    add binary search logic

--- Realization_Algorythms/src/algo/binary-search/binarySearch.js
+++ Realization_Algorythms/src/algo/binary-search/binarySearch.js
@@ -1,3 +1,28 @@
-export const binarySearch = (array, word) => {
+export const binarySearch = (array, item) => {
+    const sortedArray  = array.sort((a, b) => a - b);
+    let low = 0;
+    let high = array[array.length - 1];
+    let operations = 0;
+
+    while (low <= high) {
+        let mid = Math.floor(low + high) / 2;
+        let guess = sortedArray[mid];
+
+        if(guess === item) {
+            operations += 1;
+            return mid;
+        }
+        if (guess > item) {
+            operations += 1;
+            high = mid - 1;
+        }
+        else {
+            operations += 1;
+            low = mid + 1;
+        }
+    }
+
+    console.log(`Operations count: ${operations}`);
+
     return null;
 }
--- Realization_Algorythms/src/test/binary-search/binarySearch.test.js
+++ Realization_Algorythms/src/test/binary-search/binarySearch.test.js
@@ -2,9 +2,28 @@ import {binarySearch} from "../../algo/binary-search/binarySearch";
 import { describe, expect, it } from "@jest/globals";
 
 describe('binarySearch tests', () => {
-    it('should return null', () => {
-        const actual = binarySearch([], '');
+    it('should return null when array doe not exist number', () => {
+        //Given
+        const data = [5, 3, 2, 4, 7, 10, 12];
+        const number = 23;
 
-        expect(actual).toEqual(null);
+        //When
+        const actual = binarySearch(data, number);
+
+        //Then
+        expect(actual).toBeNull();
+    });
+
+    it('should return correct position', () => {
+        //Given
+        const data = [5, 3, 2, 4, 7, 10, 12];
+        const number = 10;
+        const position = 5;
+
+        //When
+        const actual = binarySearch(data, number);
+
+        //Then
+        expect(actual).toEqual(position);
     });
 })
