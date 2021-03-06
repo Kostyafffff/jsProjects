9d122fd144408fde6dd4140aee7f242db987eeed
kostya <kostyan68@gmail.com>
2021-03-06 23:55:12 +0200

    add description of test

--- Realization_Algorythms/src/test/insert-sorting/insertSorting.test.js
+++ Realization_Algorythms/src/test/insert-sorting/insertSorting.test.js
@@ -2,7 +2,7 @@ import {describe, expect, it} from "@jest/globals";
 import {insertSort} from "../../algo/insert-sorting/insertSorting";
 
 describe('insert sorting test', () => {
-    it('should sort array', () => {
+    it('should sort array in ascending order', () => {
         //Given
         const array = [5, 2, 10, 13, 50, 9];
         const expected = [2, 5, 9, 10, 13, 50];
