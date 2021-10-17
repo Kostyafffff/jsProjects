e0fe4e8190ca11e47634bd0bed791bb90f4787e4
kostya <kostyan68@gmail.com>
2021-10-18 00:00:10 +0300

    add form validation

--- ReactForms/src/containers/Auth/Auth.js
+++ ReactForms/src/containers/Auth/Auth.js
@@ -1,8 +1,38 @@
 import React, {Component} from 'react'
 import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'password',
+        valid: false,
+        errorMessage: 'Введите корректный password',
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
 
   loginHandler = () => {
 
@@ -16,16 +46,42 @@ export default class Auth extends Component {
     event.preventDefault()
   }
 
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}:`, event.target.value)
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+
+      return (
+          <Input
+            key={controlName + index}
+            type={control.type}
+            value={control.value}
+            valid={control.valid}
+            touched={control.touched}
+            label={control.label}
+            shouldValidate={!!control.validation}
+            errorMessage={control.errorMessage}
+            onChange={event => this.onChangeHandler(event, controlName) }
+          />
+      )
+    });
+  }
+
   render() {
+
+
     return (
       <div className={classes.Auth}>
         <div>
           <h1>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <input type="text"/>
-            <input type="text"/>
-
+            {
+              this.renderInputs()
+            }
             <Button
               type="success"
               onClick={this.loginHandler}
